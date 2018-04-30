895a1a3856c9600290c1f43804568cd0b293d8c9
Muhammed Çelik <34715591+M-Clk@users.noreply.github.com>
2018-04-30 23:31:36 +0300

    Add files via upload

---
+++ Zamanlayici/frmSayac.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Zamanlayici
+{
+    partial class frmSayac
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.tmrSayac = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.79149F));
+            this.tableLayoutPanel1.Controls.Add(this.lblSaat, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.statusStrip1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.90643F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.09357F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1223, 697);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaat.Location = new System.Drawing.Point(3, 34);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(1217, 76);
+            this.lblSaat.TabIndex = 0;
+            this.lblSaat.Text = "00:00:00.00";
+            this.lblSaat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tmrSayac
+            // 
+            this.tmrSayac.Interval = 1000;
+            this.tmrSayac.Tick += new System.EventHandler(this.tmrSayac_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 675);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1223, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 19);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(267, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmSayac
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 697);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frmSayac";
+            this.Text = "Zamanlayıcı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Timer tmrSayac;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Zamanlayici/frmSayac.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zamanlayici
+{
+    public partial class frmSayac : Form
+    {
+        public frmSayac()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            saat = 1;
+            dakika = 5;
+            saniye = 30;
+            //tmrSayac.Start();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        int saniye=0,dakika=0,saat=0,milisaniye=0;
+        int yıl, ay, hafta, gün = 0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tmrSayac.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tmrSayac.Start();
+        }
+
+        string iniyıl, iniay, inihafta;
+        private void tmrSayac_Tick(object sender, EventArgs e)
+        {
+            Azalt();   
+        }
+        void Azalt() {
+            if (yıl == 0 && ay == 0 && hafta == 0 && gün == 0 && saat == 0 && saniye == 0 && dakika == 0 && milisaniye == 0)
+            {
+                tmrSayac.Stop();
+                toolStripStatusLabel1.Text = "Geri Sayımınız Bitti - " + DateTime.Now;
+                return;
+
+            }
+            if (milisaniye==0)
+            {
+                //milisaniye = 100;
+                //Bunu artırmak zamanda sürtünmeye sebep oluyor.
+                milisaniye = 1;
+                if (saniye == 0)
+                {
+                    saniye = 60;
+                    if (dakika == 0)
+                    {
+                        dakika = 60;
+                        if (saat == 0)
+                        {
+                            saat = 24;
+                            if(gün == 0)
+                            {
+                                gün = 365;
+                                
+                                if (hafta == 0) {
+                                    hafta = gün % 52;
+                                
+                                if (ay == 0)
+                                {
+                                    ay = 12;
+                                    if (yıl == 0)
+                                    {
+                                       // yıl = 99;
+                                       //asır - century
+                                    }
+                                    yıl--;
+                                }
+                                ay--;
+                              }
+                                hafta--;
+                            }
+                            gün--;
+                            gün = gün % 7;
+                        }
+                        saat--;
+
+                    }
+                    dakika--;
+
+                }
+                saniye--;
+
+            }
+            milisaniye--;
+            
+
+
+            /*
+            if (milisaniye == 0)
+            {
+                milisaniye = 0;
+                if (saniye == 60)
+                {
+                    dakika++;
+                    saniye = 0;
+                    if (dakika % 60 == 0)
+                    {
+                        saat++;
+                        dakika = 0;
+                    }
+                }
+                saniye--;
+            }
+            milisaniye++;
+            lblSaat.Text = saat.ToString("D2") + ":" + dakika.ToString("D2") + ":" + saniye.ToString("D2") + "." + milisaniye.ToString("D2");
+            */
+
+            lblSaat.Text = gün.ToString("D1") + ":" + saat.ToString("D2") + ":" + dakika.ToString("D2") + ":" + saniye.ToString("D2") + "." + milisaniye.ToString("D2");
+        }
+        void Arttir()
+        {
+            if (milisaniye == 99)
+            {
+                milisaniye = 0;
+                if (saniye == 60)
+                {
+                    dakika++;
+                    saniye = 0;
+                    if (dakika % 60 == 0)
+                    {
+                        saat++;
+                        dakika = 0;
+                    }
+                }
+                saniye++;
+            }
+            milisaniye++;
+            lblSaat.Text = saat.ToString("D2") + ":" + dakika.ToString("D2") + ":" + saniye.ToString("D2") + "." + milisaniye.ToString("D2");
+        }
+    }
+}
